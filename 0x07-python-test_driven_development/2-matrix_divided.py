28c6dd31f68849bb81024f9a9032683cabed0c51
Caleb <caaleb02@gmail.com>
2022-10-19 05:33:56 -0700

    2 0x07.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -27,4 +27,4 @@ integers/floats")
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for a in matrix]
+    return [[round(i / div, 2) for i in a] for a in matrix]
